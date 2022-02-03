066176eb61cfefe1940bc79014bdb7ca53df7222
sprocketsullivan <toelch@gmail.com>
2022-02-03 08:57:42 +0000

    Initial commit

--- initialise.R
+++ initialise.R
@@ -1,2 +1,4 @@
 install.packages("devtools")
 devtools::install_github("https://github.com/benmarwick/rrtools")
+#enter the name of the package here. Will alo be in the Github
+rrtools::use_compendium("rrr")
